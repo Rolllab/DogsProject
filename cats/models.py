0c0d84f1aa8dfbbef0504b606680f44abc6da4bf
Дмитрий Волков <roskey@mail.ru>
2025-03-23 19:54:58 +0300

    1) Добавлена модель Cat; 2) Созданы и применены миграции

--- cats/models.py
+++ cats/models.py
@@ -12,3 +12,23 @@ class Bread(models.Model):
     class Meta:
         verbose_name = 'bread'
         verbose_name_plural = 'breads'
+
+
+class Cats(models.Model):
+    name = models.CharField(max_length=250, verbose_name='cat name')
+    bread = models.ForeignKey(Bread, on_delete=models.CASCADE, verbose_name='bread')
+    photo = models.ImageField(upload_to='dogs/', **NULLABLE, verbose_name='image')
+    birth_date = models.DateField(**NULLABLE,verbose_name='birth_date')
+
+    def __str__(self):
+        return f'{self.name} ({self.bread})'
+
+    class Meta:
+        verbose_name = 'cat'
+        verbose_name_plural = 'cats'
+        # abstract = True
+        # app_label = 'cats'
+        # ordering = [-1]
+        # permissions = []
+        # db_table = 'cat_table'
+        # get_latest_by = 'birth_date'
