3cca4359e70a9bf0907ff62d6330203c08cb0598
Дмитрий Волков <roskey@mail.ru>
2025-03-23 19:31:03 +0300

    1) Приложение cats добавлено в настройки; 2) Добавлена модель породы; 3) Созданы и применены миграции

--- cats/models.py
+++ cats/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+from users.models import NULLABLE
+
+class Bread(models.Model):
+    name = models.CharField(max_length=100, verbose_name='bread')
+    description = models.CharField(max_length=1000, verbose_name='description', **NULLABLE)
+
+    def __str__(self):
+        return f'{self.name}'
+
+    class Meta:
+        verbose_name = 'bread'
+        verbose_name_plural = 'breads'
