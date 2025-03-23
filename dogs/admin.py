675b0edc6865d2f06a54c94c0eb0ede3ef05ed4c
Дмитрий Волков <roskey@mail.ru>
2025-03-23 20:53:59 +0300

    1) Модели Bread и Dog зарегистрированы в админке; 2) Сделаны миграции

--- dogs/admin.py
+++ dogs/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from dogs.models import Bread, Dog
+
+@admin.register(Bread)
+class BreadAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'name', )
+    ordering = ('pk', )
+
+@admin.register(Dog)
+class DogAdmin(admin.ModelAdmin):
+    list_display = ('name', 'bread',)
+    list_filter = ('bread', )
+    ordering = ('name', )
