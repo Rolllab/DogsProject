1b1912e2e1d0ee807bc2915c6066a2586dc2ca39
Дмитрий Волков <roskey@mail.ru>
2025-03-23 20:54:58 +0300

    1) Модели Bread и Cat зарегистрированы в админке; 2) Сделаны миграции

--- cats/admin.py
+++ cats/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from cats.models import Bread, Cat
+
+@admin.register(Bread)
+class BreadAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'name', )
+    ordering = ('pk', )
+
+@admin.register(Cat)
+class DogAdmin(admin.ModelAdmin):
+    list_display = ('name', 'bread',)
+    list_filter = ('bread', )
+    ordering = ('name', )
