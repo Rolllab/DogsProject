47ac0b4f5eb86dcf75b5fb05e4276d94f4df0b10
Дмитрий Волков <roskey@mail.ru>
2025-03-26 23:04:25 +0300

    Добавил URL для create

--- dogs/urls.py
+++ dogs/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from dogs.views import index, breed_dogs_list_view, breeds_list_view, dogs_list_view
+from dogs.views import index, breed_dogs_list_view, breeds_list_view, dogs_list_view, dog_create_view
 from dogs.apps import DogsConfig
 
 app_name = DogsConfig.name
@@ -11,4 +11,5 @@ urlpatterns = [
     path('breeds/<int:pk>/dogs/', breed_dogs_list_view, name='breed_dogs'),
 
     path('dogs/', dogs_list_view, name='dogs_list'),
+    path('dogs/create/', dog_create_view, name='dog_create'),
 ]
