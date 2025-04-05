f65fe5ef7f3bb75a156f83b9e511f05932e5a86c
Дмитрий Волков <roskey@mail.ru>
2025-04-05 16:55:47 +0300

    1) Корректировка моделей; 2) Созданы и применены миграции.

--- users/models.py
+++ users/models.py
@@ -6,13 +6,13 @@ NULLABLE = {'blank': True, 'null': True}
 
 class User(AbstractUser):
     username = None
-    email = models.EmailField(unique=True, verbose_name='email')
-    first_name = models.CharField(max_length=150, verbose_name='first_name', default='Anonymous')
-    last_name = models.CharField(max_length=150, verbose_name='last_name', default='Anonym')
-    phone = models.CharField(max_length=35, verbose_name='phone number', **NULLABLE)            # не обязательное поле
-    telegram = models.CharField(max_length=150, verbose_name='telegram username', **NULLABLE)   # не обязательное поле
-    avatar = models.ImageField(upload_to='users/', verbose_name='avatar', **NULLABLE)
-    is_active = models.BooleanField(default=True, verbose_name='active')
+    email = models.EmailField(unique=True, verbose_name='Эл. почта')
+    first_name = models.CharField(max_length=150, verbose_name='Имя', default='Anonymous')
+    last_name = models.CharField(max_length=150, verbose_name='Фамилия', default='Anonym')
+    phone = models.CharField(max_length=35, verbose_name='Телефон', **NULLABLE)            # не обязательное поле
+    telegram = models.CharField(max_length=150, verbose_name='Телеграм', **NULLABLE)   # не обязательное поле
+    avatar = models.ImageField(upload_to='users/', verbose_name='Аватар', **NULLABLE)
+    is_active = models.BooleanField(default=True, verbose_name='Активность')
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
