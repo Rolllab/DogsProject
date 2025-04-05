c9d3f5424055be6397f40e5c56e1f3a385e6328b
Дмитрий Волков <roskey@mail.ru>
2025-04-05 17:00:41 +0300

    1) Написан класс StyleFormMixin; 2) StyleFormMixin добавлен к формам.

--- users/forms.py
+++ users/forms.py
@@ -2,7 +2,16 @@ from django import forms
 
 from users.models import User
 
-class UserRegisterForm(forms.ModelForm):
+
+class StyleFormMixin:
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for filed_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control'
+
+
+
+class UserRegisterForm(StyleFormMixin, forms.ModelForm):
     password = forms.CharField(label='Пароль', widget=forms.PasswordInput)
     password2 = forms.CharField(label='Подтвердите пароль', widget=forms.PasswordInput)
 
@@ -17,12 +26,12 @@ class UserRegisterForm(forms.ModelForm):
         return cd['password2']
 
 
-class UserLoginForm(forms.Form):
+class UserLoginForm(StyleFormMixin, forms.Form):
     email = forms.EmailField()
     password = forms.CharField(label='Пароль', widget=forms.PasswordInput)
 
 
-class UserForm(forms.ModelForm):
+class UserForm(StyleFormMixin, forms.ModelForm):
     class Meta:
         model = User
         # fields = ('email', 'first_name', 'last_name', 'phone')    # Один из вариантов записи
