9697502c331fd699582de762ba9d0ebc2061002f
Дмитрий Волков <roskey@mail.ru>
2025-04-06 23:36:59 +0300

    1) Добавлена валидация даты рождения питомца; 2) Добавлено фото новой собаки.

--- dogs/forms.py
+++ dogs/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from datetime import datetime
 
 from dogs.models import Dog
 from users.forms import StyleFormMixin
@@ -8,3 +9,10 @@ class DogForm(StyleFormMixin, forms.ModelForm):
     class Meta:
         model = Dog
         exclude = ('owner', )
+
+    def clean_birth_date(self):
+        cleaned_data = self.cleaned_data['birth_date']
+        now_year = datetime.now().year
+        if now_year - cleaned_data.year > 35:
+            raise forms.ValidationError('Собака должна быть моложе 35 лет')
+        return cleaned_data
